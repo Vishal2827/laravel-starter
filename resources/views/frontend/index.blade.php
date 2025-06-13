0cf93787340b05180e7e75198870e669db0c2159
Vishal Parjapat <159058540+Vishal2827@users.noreply.github.com>
2025-06-13 15:36:45 +0530

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -87,7 +87,7 @@
                 </h1>
 
                 <p class="mb-8 leading-relaxed">
-                    Vishal Parjapat,
+                    Vishu Parjapat ,
                     Laravel Starter.
                 </p>
             </div>
